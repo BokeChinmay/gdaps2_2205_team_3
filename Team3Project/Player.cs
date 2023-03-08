9f20f5e8f21ed5588985be2932f890fe80978c22
BokeChinmay <crg2205@rit.edu>
2023-03-08 11:54:00 -0500

    Rip.

--- Team3Project/Player.cs
+++ Team3Project/Player.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,19 +14,28 @@ namespace Team3Project
         //Fields
         private Texture2D playerTexture;
         private Vector2 playerLoc;
+        private KeyboardState kbState;
 
         //Constants for Source Rectangle
         const int PlayerRectOffsetY = 8;
         const int PlayerRectHeight = 20;
-
         public Player(int health, int moveSpeed) : base(health, moveSpeed)
         {
 
-        }
+            if (kbState.IsKeyDown(Keys.S))
+            {
+                playerPos.X++;
+            }
 
-        protected override void Move()
-        {
+            if (kbState.IsKeyDown(Keys.D))
+            {
+                playerPos.Y++;
+            }
 
+            if (kbState.IsKeyDown(Keys.W))
+            {
+                playerPos.Y--;
+            }
         }
 
         public void TakeDamage(int amount)
