fa7fe1bef689addb46e503c06433db9acac12381
Owen Gahagan <opg4403@g.rit.edu>
2023-04-11 18:29:12 -0400

    Added comments to ScoreDisplay

--- Team3Project/Stage_Stuff/ScoreDisplay.cs
+++ Team3Project/Stage_Stuff/ScoreDisplay.cs
@@ -11,25 +11,37 @@ namespace Team3Project.Stage_Stuff
 {
     internal class ScoreDisplay : UIObject
     {
+        // Field declarations
         private SpriteFont font;
         private int level;
 
+        // Property for level
         public int Level
         {
             get { return level; }
             set { level = value; }
         }
 
+        // Parameterized constructor
         public ScoreDisplay(SpriteFont font) : base (1700, 75, 135, 740)
         {
             this.font = font;
         }
 
+        /// <summary>
+        /// Updates the level display based on the value that the player has stored
+        /// </summary>
+        /// <param name="player"></param>
         public void Update(Player player)
         {
             level = player.Level;
         }
 
+        /// <summary>
+        /// Draws the display
+        /// </summary>
+        /// <param name="spriteBatch"></param>
+        /// <param name="spriteEffects"></param>
         public override void Draw(SpriteBatch spriteBatch, SpriteEffects spriteEffects)
         {
             spriteBatch.DrawString(font, $"Level: {level}", new Vector2(1355, 25), Color.LightGreen);
